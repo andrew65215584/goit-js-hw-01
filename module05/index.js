0898b450e66dabc37191c41b5bd7ff1fcaa0d094
AndrewZ27 <zabolotckiy@gmail.com>
2020-07-27 16:26:41 +0300

    залил 5 дз

---
+++ module05/index.js
@@ -0,0 +1,132 @@
+//? task 1
+
+// const Account = function ({ login, email }) {
+
+//     this.login = login;
+//     this.email = email;
+
+// };
+
+// Account.prototype.getInfo = function () {
+//     console.log(`
+//     login: ${this.login}
+//     email: ${this.email} `);
+
+// };
+
+// const mango = new Account({
+//     login: 'Mangozedog',
+//     email: 'mango@dog.woof',
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//     login: 'Poly',
+//     email: 'poly@mail.com',
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+//? task 2
+
+// class User {
+// 	constructor({ name, age, followers }) {
+// 		this.name = name;
+// 		this.age = age;
+// 		this.followers = followers;
+// 	}
+
+// 	getInfo() {
+// 		console.log(`
+//       User ${this.name}  is ${this.age} years old 
+//       and has ${this.followers} followers
+//     `);
+// 	}
+// }
+
+// const mango = new User({
+// 	name: 'Mango',
+// 	age: 2,
+// 	followers: 20
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+// 	name: 'Poly',
+// 	age: 3,
+// 	followers: 17
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+//? task 3
+
+// class Storage {
+// 	constructor(array) {
+// 		this.items = array;
+// 	}
+
+// 	getItems() {
+// 		return this.items;
+// 	}
+
+// 	addItem(item) {
+// 		this.items.push(item);
+// 	}
+
+// 	removeItem(item) {
+// 		for (let i = 0; i < this.items.length; i += 1) {
+// 			if (this.items[i] === item) {
+// 				this.items.splice([ i ], 1);
+// 			}
+// 		}
+// 	}
+// }
+
+// const storage = new Storage([ 'Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор' ]);
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+//? task 4
+
+// class StringBuilder {
+// 	constructor(str) {
+// 		this._value = str;
+// 	}
+
+// 	get value() {
+// 		return this._value;
+// 	}
+
+// 	append(str) {
+// 		this._value = this._value + str;
+// 	}
+
+// 	prepend(str) {
+// 		this._value = str + this._value;
+// 	}
+
+// 	pad(str) {
+// 		this._value = str + this._value + str;
+// 	}
+// }
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
--- training/index.js
+++ training/index.js
@@ -1,163 +1,23 @@
-// Напишите функцию isEmpty(obj), которая возвращает true,
-// если у объекта нет свойств, иначе false.
+class SHOP {
+	constructor(item, quantity) {
+		this.item = item;
+		this.quantity = quantity;
+	}
 
-// let schedule = {};
+	showBuy(item) {
+		console.log(`Вы купили ${this.item}`);
+	}
 
-// function isEmpty(obj) {
+	buy(item, quantity) {
+		this.quantity += quantity;
+	}
+}
 
-//    const keys = Object.keys(obj)
+const myShop = new SHOP('socks', 10);
 
-//     if (keys.length === 0) {
-//         return true;
-//     } else {
-//         return false
-//     }
+console.log(myShop);
 
-// }
 
-// // alert(isEmpty(schedule)); // true
 
-// schedule["8:30"] = "get up";
 
-// alert(isEmpty(schedule)); // false
 
-// ? task 2
-
-// let salaries = {
-// 	john: 100,
-// 	ann: 160,
-// 	pete: 130
-// };
-
-// const sumSalaries = function(obj) {
-// 	let sum = 0;
-
-// 	for (let keys in obj) {
-// 		sum += obj[keys];
-// 	}
-
-// 	return sum;
-// };
-
-// alert(sumSalaries(salaries));
-
-// ? task 3
-// #4
-// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
-
-// let menu = {
-//     width: 200,
-//     height: 300,
-//     title: "My menu",
-//     height: null,
-//     weight: undefined,
-// };
-
-// const multiplyNumeric = function (object) {
-
-//     for (let keys in object) {
-
-//         if (typeof object[keys] == "number")
-//             object[keys] = object[keys] * 2;
-//     }
-
-//     return object;
-// }
-
-// console.log(multiplyNumeric(menu));
-
-//? task 4 object
-
-// const object = {
-// 	name: 'Петр',
-// 	surname: 'Петров',
-// 	age: '20 лет'
-// };
-
-// const { name = "Антоном", surname = "Анонимович", age = '? лет' } = object;
-// console.log(object);
-// console.log(name);
-
-// ? 5 object
-
-// function fizzBazz(n) {
-//     for (let i = 1; i <= n; i++) {
-
-//         if (i % 3 === 0 && i % 5 === 0) {
-//             console.log('fizzBuzz');
-//         } else if (i % 3 === 0) {
-//             console.log('fizz');
-//         } else if (i % 5 === 0) {
-//             console.log('buzz');
-//         } else {
-//             console.log(i);
-//         }
-
-// 	}
-// }
-
-// fizzBazz(133);
-
-// const fizzBuzz = function (n) {
-//     for (let i = 1; i <= n; i += 1) {
-//         if (i % 3 && i % 5) {
-//             console.log(i);
-//         } else if (i % 3) {
-//             console.log("buzz");
-//         } else if (i % 5) {
-//             console.log("fizz");
-//         } else {
-//             console.log("fizzbuzz");
-//         }
-//     }
-// };
-// fizzBuzz(15);
-
-// ? task 6 object
-
-// Создайте объект calculator (калькулятор) с тремя методами:
-
-// read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
-// sum() (суммировать) возвращает сумму сохранённых значений.
-// mul() (умножить) перемножает сохранённые значения и возвращает результат.
-
-// let calculator = {
-//     read: function (...params) {
-//         for (let i = 0; i < params.length; i++) {
-//             this[i] = params[i];
-//         }
-//     },
-//     sum: function () {
-//         let arr = Object.values(calculator);
-//         let sum = 0;
-//         for (let el of arr) {
-//             if (typeof el === "number") {
-//                 sum += el;
-//             }
-//         }
-//         return sum;
-//     },
-//     mul: function () {
-//         let arr = Object.values(calculator);
-//         let mul = 1;
-//         for (let el of arr) {
-//             if (typeof el === "number") {
-//                 mul *= el;
-//             }
-//         }
-//         return mul;
-//     }
-// };
-// calculator.read(3, 4, 5, 6, 7, 8, 10);
-// alert(calculator.sum());
-// alert(calculator.mul());
-// console.log(calculator);
-
-const target = {
-	id: 24,
-	isOnline: true,
-	...{ name: 'Mango', age: 2 },
-	...{ age: 7, isOnline: false }
-};
-
-console.log(target);
